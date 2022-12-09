514a83115c7c9520f3a0a249b27d2982cfec7cf0
SueLetty <xiuxiang1129@gmail.com>
2022-12-09 15:24:00 -0800

    updated Game class, AB#137

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,37 +1,71 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
-
-private String location;
-
-
-
-public void station() {
-
-}
-
-public String welcomeMessage() {
- return null;
-}
-
-public String helpMessage() {
-  return null;
-}
-
-public String tryAgainMessage() {
-  return null;
-}
-
-public void directions() {
-
-}
+  private Player player;
+  private String welcomeMessage;
+  private List<String> stations;
+  private String helpMessage;
+  private String tryAgainMessage;
+//  private Directions direction;
 
 
-public String getLocation() {
-    return location;
+  public void Game(Player player, String welcomeMessage, String helpMessage, String tryAgainMessage ) {
+    this.player = player;
+    this.welcomeMessage = welcomeMessage;
+    this.helpMessage = helpMessage;
+    this.tryAgainMessage = tryAgainMessage;
+    this.stations = new ArrayList<>();
+//    this.direction = direction;
+//
+  }
+  public void tryAgain(){
+
+  }
+  public void quit(){
+
+  }
+  public void help(){
+
+  }
+  public void display(){
+
   }
 
 
-public void setLocation(String location) {
-    this.location = location;
+  public Player getPlayer() {
+    return player;
   }
+
+  public String getWelcomeMessage() {
+    return welcomeMessage;
+  }
+
+  public List<String> getStations() {
+    return stations;
+  }
+
+  public String getHelpMessage() {
+    return helpMessage;
+  }
+
+  public String getTryAgainMessage() {
+    return tryAgainMessage;
+  }
+
+//  public Directions getDirection() {
+//    return direction;
+//  }
+//
+//  public void setDirection(Directions direction) {
+//    this.direction = direction;
+//  }
+
+  public void setStations(String station) {
+    this.stations.add(station);
+  }
+
 }
+
+
