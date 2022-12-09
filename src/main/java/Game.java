789f8dde5f3f3fb54a227bca235f721a70ce9064
SaganJackson <sagan.jacksonmoses@gmail.com>
2022-12-09 14:20:52 -0800

    Added Game class and Maven framework, AB#137

---
+++ src/main/java/Game.java
@@ -0,0 +1,37 @@
+public class Game {
+
+
+private String location;
+
+
+
+public void station() {
+
+}
+
+public String welcomeMessage() {
+ return null;
+}
+
+public String helpMessage() {
+  return null;
+}
+
+public String tryAgainMessage() {
+  return null;
+}
+
+public void directions() {
+
+}
+
+
+public String getLocation() {
+    return location;
+  }
+
+
+public void setLocation(String location) {
+    this.location = location;
+  }
+}
