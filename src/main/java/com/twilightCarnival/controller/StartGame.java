6361a335ec8711ed678a985d00addd058c7c2611
ejbanda <bandaericj@gmail.com>
2022-12-13 12:11:27 -0600

    added operation logic, AB#144

--- src/main/java/com/twilightCarnival/controller/StartGame.java
+++ src/main/java/com/twilightCarnival/controller/StartGame.java
@@ -13,14 +13,14 @@ public class StartGame {
     if(input.nextLine().equals("y")){
       System.out.println("Game started!");
       game.display();
-      game.status();
+      game.status();  //need to override?
       userInput();
     }
     game.quit();
   }
 
 
-  public void userInput(){
+  private void userInput(){
     InputValidator validator = new InputValidator();
     String[] validInput = new String[2];
     Scanner input = new Scanner(System.in);
@@ -35,11 +35,30 @@ public class StartGame {
         game.quit();
       } else if (validator.isValid(userChoice)) {
         validInput = validator.getInput();
+        operation(validInput[0], validInput[1]);
+        game.status();
         // verb is in validInput[0]
         // noun is in validInput[1]
         // use validInput to do something
         continue;
       }
+    }
+  }
+
+  private void operation(String verb, String noun){
+    if (verb.equals("open") && noun.equals("map")){
+      if (game.getPlayer().hasMap()){
+        game.viewMap();
+      }else {
+        System.out.println("You do not have a map.");
+      }
+    } else if (verb.equals("pickup")) {
+      // TODO: 12/13/2022 get current location and check for items to pickup
+      // call status here?, message below might not display.
+      System.out.printf("You picked up the %s and added it to your inventory", noun);
+      game.getPlayer().setInventory(noun);
+    } else if (verb.equals("go")) {
+      // TODO: 12/13/2022 change locations
     }
   }
 }
