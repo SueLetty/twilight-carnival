42a3276169d71112e4a36d2ca0310829bb3bd1c8
SueLetty <xiuxiang1129@gmail.com>
2022-12-14 12:26:42 -0800

    added Script class to load script JSON file

---
+++ src/main/java/com/twilightCarnival/model/Script.java
@@ -0,0 +1,70 @@
+package com.twilightCarnival.model;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class Script {
+  private Script script;
+  private String introduction;
+  private String directions;
+  private String toolMessage;
+  private String winMessage;
+  private String help;
+  private String playAgainMessage;
+
+  public Script() {
+
+  }
+
+  public Script(String introduction, String directions, String toolMessage, String winMessage, String help, String playAgainMessage){
+    this.introduction = introduction;
+    this.directions = directions;
+    this.toolMessage = toolMessage;
+    this.winMessage = winMessage;
+    this.help = help;
+    this.playAgainMessage = playAgainMessage;
+  }
+  public void load(){
+    try{
+      String saveFile = "script.json";
+      Reader reader = Files.newBufferedReader(Paths.get(saveFile));
+      Gson gson = new Gson();
+      script = gson.fromJson(reader, Script.class);
+      System.out.println(script.getHelp());
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+  public String getIntroduction() {
+    return introduction;
+  }
+
+  public String getDirections() {
+    return directions;
+  }
+
+  public String getToolMessage() {
+    return toolMessage;
+  }
+
+  public String getWinMessage() {
+    return winMessage;
+  }
+
+  public String getHelp() {
+    return help;
+  }
+
+  public String getPlayAgainMessage() {
+    return playAgainMessage;
+  }
+
+  public Script getScript() {
+    return script;
+  }
+}
