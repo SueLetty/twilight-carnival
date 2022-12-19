b2bdc085142eebff131f7f07d1775448fdeab978
SueLetty <xiuxiang1129@gmail.com>
2022-12-19 12:36:22 -0800

    added SetMapTest class, modified all the test classes, AB#145

--- test/com/twilightCarnival/model/MonsterTest.java
+++ test/com/twilightCarnival/model/MonsterTest.java
@@ -2,7 +2,6 @@ package com.twilightCarnival.model;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -10,20 +9,22 @@ public class MonsterTest {
   public static Monster monster;
 
   @BeforeClass
-  public static void beforeClass() throws Exception {
+  public static void beforeClass(){
     monster = new Monster("Balloon Dog Monster", "needle", "You carried by other balloon monsters", "You defeated the monster!","gold key");
   }
 
   @Test
-  public void setStatus() {
+  public void testSetStatus() {
     assertTrue(monster.isAlive());
     monster.setStatus(false);
     assertFalse(monster.isAlive());
   }
 
   @Test
-  public void getName() {
-    assertTrue("Balloon Dog Monster".equals(monster.getName()));
+  public void testGetName() {
+    String expected = "Balloon Dog Monster";
+    String actual = monster.getName();
+    assertEquals(expected, actual);
   }
 
   @Test
@@ -31,32 +32,40 @@ public class MonsterTest {
     assertFalse(monster.isAlive());
     monster.setStatus(true);
     assertTrue(monster.isAlive());
-    ;
+
   }
 
   @Test
-  public void getWeakness() {
-    assertTrue("needle".equals(monster.getWeakness()));
+  public void testGetWeakness() {
+    String expected = "needle";
+    String actual = monster.getWeakness();
+    assertEquals(expected, actual);
   }
 
   @Test
-  public void getKey() {
-    assertTrue("gold key".equals(monster.getKey()));
+  public void testGetKey() {
+    String expected = "gold key";
+    String actual = monster.getKey();
+    assertEquals(expected, actual);
   }
 
   @Test
-  public void setKey() {
+  public void testSetKey() {
     monster.setKey(null);
     assertNull(monster.getKey());
   }
 
   @Test
-  public void getWinMessage() {
-    assertTrue("You defeated the monster!".equals(monster.getWinMessage()));
+  public void testGetWinMessage() {
+    String expected = "You defeated the monster!";
+    String actual = monster.getWinMessage();
+    assertEquals(expected, actual);
   }
 
   @Test
-  public void getLostMessage() {
-    assertTrue("You carried by other balloon monsters".equals(monster.getLostMessage()));
+  public void testGetLostMessage() {
+    String expected = "You carried by other balloon monsters";
+    String actual = monster.getLostMessage();
+    assertEquals(expected, actual);
   }
 }
